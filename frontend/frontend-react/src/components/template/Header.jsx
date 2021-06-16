18d0ac37693be4e7424603e27e8c5088843d7413
Leonardo <leocoelho.pi@gmail.com>
2021-06-15 21:04:00 -0300

    add position, heart event, notifications in header

--- frontend/frontend-react/src/components/template/Header.jsx
+++ frontend/frontend-react/src/components/template/Header.jsx
@@ -1,18 +1,29 @@
 import './Header.css'
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props =>
-    <header className="header">
-        <div className="h2 font-weight-bold">Instagram</div>
-        <div className="input-group col-3">
-            <span className="input-group-text"><i className="fa fa-search"></i></span>
-            <input type="text" class="form-control" id="formGroupExampleInput"
-                placeholder={`Buscar`}></input>
-        </div>
-        <div>
-            <i className="icons fa fa-home"></i>
-            <i className="icons fa fa-comments"></i>
-            <i className="icons fa fa-compass"></i>
-            <i className="icons fa fa-heart teste"></i>
-        </div>
-    </header>
+export default class Main extends Component {
+    handle = (e) => {
+        e.target.classList.toggle("red");
+    }  
+
+    render() {  
+        return (
+            <header className="header">
+                <div className="h2 font-weight-bold">Instagram</div>
+                <div className="input-group col-3">
+                    <span className="input-group-text"><i className="fa fa-search"></i></span>
+                    <input type="text" class="form-control" id="formGroupExampleInput"
+                        placeholder={`Buscar`}></input>
+                </div>
+                <div>
+                    <i className="icons fa fa-home"></i>
+                    <i className="icons fa fa-comments">
+                        <div className="notifications badge">2</div>
+                    </i>
+                    <i className="icons fa fa-compass"></i>
+                    <i onClick={(e) => this.handle(e)} className="icons fa fa-heart px-1"></i>
+                </div>
+            </header>
+        )
+    }
+}
